d553a467154bb92dbdb285be49438e88a3bae970
Vacca <vacca1089@gmail.com>
2024-07-29 10:16:27 +0200

    Update Config.cs

--- Plugin/Config.cs
+++ Plugin/Config.cs
@@ -9,7 +9,7 @@ namespace WelcomeMessage
         public bool IsEnabled { get; set; } = true;
 
         [Description("Messaggio di benvenuto personalizzato.")]
-        public string WelcomeMessage { get; set; } = "Benvenuto nel server!";
+        public string Broadcast { get; set; } = "Benvenuto nel server!";
 
         [Description("Tempo di visualizzazione del messaggio in secondi.")]
         public ushort MessageDuration { get; set; } = 15;
